3fce6e1fa0a855ab883c061d34656a15d8262f1e
BgNikoto11 <148450284+BgNikoto11@users.noreply.github.com>
2024-05-29 13:13:00 +0300

    Added validation messages
    Errors sent from the login validation file, which are used to display error messages on this page.

---
+++ front_end/markup/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+    if(isset($_GET['error'])){
+        if($_GET['error'] == 'emptyfields'){
+            echo '<p style="color: red;">Please fill in all fields.</p>';
+        }
+        elseif($_GET['error'] == 'invalidlogin'){
+            echo '<p style="color: red;">Invalid username or password.</p>';
+        }
+    }
+    ?>
+
+    <form id="login-form" method="post" action="../../back_end/login-form-handler.inc.php">
+
+        <label for="username">Username</label><br>
+        <input type="text" name="username"><br>
+
+        <label for="password">Password</label><br>
+        <input type="text" name="password"><br>
+
+        <input type="submit" value="Login">
+
+    </form>
+    
+</body>
+</html>
